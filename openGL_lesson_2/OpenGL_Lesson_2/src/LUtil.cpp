751502cf9f88a2ef87247fd613f94344aaa2e82d
Geoff Wild <boris@zerohour-productions.net>
2013-10-28 17:36:35 -0500

    Added the initGL() function

--- openGL_lesson_2/OpenGL_Lesson_2/src/LUtil.cpp
+++ openGL_lesson_2/OpenGL_Lesson_2/src/LUtil.cpp
@@ -13,5 +13,27 @@ bool initGL()
 	glLoadIdentity();
 	glOrtho( 0.0, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0, 1.0, -1.0 );
 
+	//initialize modelview matrix
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+
+	//initialize clear color
+	glClearColor( 0.f, 0.f, 0.f, 1.f );
+
+	//check for error
+	GLenum error = glGetError();
+	if ( error != GL_NO_ERROR )
+	{
+		printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+		return false;
+	}
+
 	return true;
 }
+// end initGL()
+
+void update()
+{
+
+}
+//end update()

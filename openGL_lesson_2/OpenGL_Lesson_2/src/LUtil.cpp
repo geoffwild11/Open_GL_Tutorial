2f134b751da21bad7c3cc7fd3e31b386eb90666f
Geoff Wild <boris@zerohour-productions.net>
2013-10-28 17:54:26 -0500

    Setup my render function. Taking another break.

--- openGL_lesson_2/OpenGL_Lesson_2/src/LUtil.cpp
+++ openGL_lesson_2/OpenGL_Lesson_2/src/LUtil.cpp
@@ -1,7 +1,7 @@
 #include "LUtil.h"
 
 // The current color rendering mode
-int gColormode = COLOR_CYAN;
+int gColorMode = COLOR_CYAN;
 
 // Projection scale
 GLfloat gProjectionScale = 1.f;
@@ -37,3 +37,49 @@ void update()
 
 }
 //end update()
+
+void render()
+{
+	//clear color buffer
+	glClear( GL_COLOR_BUFFER_BIT );
+
+	//reset modelview matrix
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+
+	//move to center of screen
+	glTranslatef( SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f, 0.f);
+
+	if (gColorMode == COLOR_CYAN)
+	{
+		//Solid Cyan
+		glBegin( GL_QUADS );
+			glColor3f( 0.f, 1.f, 1.f);
+			glVertex2f( -50.f, -50.f );
+			glVertex2f(  50.f, -50.f );
+			glVertex2f(  50.f,  50.f );
+			glVertex2f( -50.f,  50.f );
+		glEnd();
+	}
+
+	else
+	{
+		//RYGB Mix
+		glBegin( GL_QUADS );
+			glColor3f( 1.f, 0.f, 0.f ); glVertex2f( -50.f, -50.f );
+			glColor3f( 1.f, 1.f, 0.f ); glVertex2f(  50.f, -50.f );
+			glColor3f( 0.f, 1.f, 0.f ); glVertex2f(  50.f,  50.f );
+			glColor3f( 0.f, 0.f, 1.f ); glVertex2f( -50.f,  50.f );
+		glEnd();
+	}
+
+	//Update screen
+	glutSwapBuffers();
+}
+//end render()
+
+void handleKeys()
+{
+
+}
+//end handleKeys()
